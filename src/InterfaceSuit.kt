5061e0c9cdcea921cfd0e2e9f16ae390d2aaf75c
mfrizki1 <mfrizki1>
2020-12-18 20:25:35 +0800

    Added InterfaceSuit

---
+++ src/InterfaceSuit.kt
@@ -0,0 +1,3 @@
+interface InterfaceSuit {
+    fun doSuit(pemain_1: String, pemain_2: String): String
+}
--- src/Suit.kt
+++ src/Suit.kt
@@ -1,31 +1,34 @@
-open class Suit  {
-    open fun doSuit(pem_1: String, pem_2:String): String{
-        return when (pem_1) {
-            "gunting" -> {
-                return when (pem_2) {
-                    "gunting" -> "DRAW!!!"
-                    "batu" -> "Pemain 2 WIN"
-                    "kertas" -> "Pemain 1 WIN"
+open class Suit: InterfaceSuit  {
+    private val gunting = "gunting"
+    private val batu = "batu"
+    private val kertas = "kertas"
+    override fun doSuit(pemain_1: String, pemain_2:String): String {
+        return when (pemain_1) {
+            gunting -> {
+                return when (pemain_2) {
+                    gunting -> "DRAW!!!"
+                    batu -> "Pemain 2 WIN"
+                    kertas -> "Pemain 1 WIN"
                     else -> "NULL"
                 }
             }
             "batu"->{
-                return when(pem_2){
-                    "gunting" -> "Pemain 1 WIN"
-                    "batu" -> "DRAW!!!"
-                    "kertas" -> "Pemain 2 WIN"
+                return when(pemain_2){
+                    gunting -> "Pemain 1 WIN"
+                    batu -> "DRAW!!!"
+                    kertas -> "Pemain 2 WIN"
                     else->"NULL"
                 }
             }
             "kertas"->{
-                return when(pem_2){
-                    "batu" -> "Pemain 1 WIN"
-                    "kertas" -> "DRAW!!!"
-                    "gunting" -> "Pemain 2 WIN"
+                return when(pemain_2){
+                    batu -> "Pemain 1 WIN"
+                    kertas -> "DRAW!!!"
+                    gunting -> "Pemain 2 WIN"
                     else->"NULL"
                 }
             }
-            else -> "NULL"
+            else -> "HARUS PILIH GUNTING / KERTAS / BATU"
         }
 
     }
